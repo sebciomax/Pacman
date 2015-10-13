23f3cd727c6a07b1dde9d288c001222923c3d3df
sebciomax <sebciomax@yahoo.com>
2015-10-13 11:53:51 -0400

    Create SoundEffect.java

---
+++ SoundEffect.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.URL;
+import javax.sound.sampled.*;
+   
+
+public enum SoundEffect {
+   SUP("baby.wav"),   
+   PELL("squaw.wav");         
+   
+  
+   public static enum Volume {
+      MUTE, LOW, MEDIUM, HIGH
+   }
+   
+   public static Volume volume = Volume.LOW;
+   
+
+   private Clip clip;
+   
+   
+   SoundEffect(String soundFileName) {
+      try {
+         
+         URL url = this.getClass().getClassLoader().getResource(soundFileName);
+        
+         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
+         
+         clip = AudioSystem.getClip();
+      
+         clip.open(audioInputStream);
+      } catch (UnsupportedAudioFileException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (LineUnavailableException e) {
+         e.printStackTrace();
+      }
+   }
+   
+   
+   public void play() {
+      if (volume != Volume.MUTE) {
+         if (clip.isRunning())
+            clip.stop();   
+         clip.setFramePosition(0); 
+         clip.start();     
+      }
+   }
+   
+   
+   static void init() {
+      values(); 
+   }
+}
